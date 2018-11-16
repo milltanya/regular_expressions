1afb5a9c12d3a87cfe5267161c7d4537ae68f5fd
Tatiana <tanya.miller.perm@gmail.com>
2018-11-17 00:05:51 +0300

    build_nfa, delete_empty_edges, build_dfa; new tests; list -> namedtuple; 'Error in the Reverse...' -> f'Error ...'; for i in range... -> for cymbol in reg

--- automaton.py
+++ automaton.py
@@ -1,98 +1,115 @@
-class Error(Exception):
-    pass
+from collections import namedtuple
 
 
-class RPNError(Error):
+class RPNError(Exception):
     def __init__(self, regular_expression, message):
         self.regular_expression = regular_expression
         self.message = message
 
+    def __str__(self):
+        return f"Error in the Reverse Polish notation " \
+               f"'{self.regular_expression}': {self.message}"
 
-def build_automaton(reg):
-    if reg is '':
-        return [{}]
+
+Edge = namedtuple('Edge', ['symbol', 'to'])
+Pair = namedtuple('Pair', ['start', 'end'])
+NFA = namedtuple('Automaton', ['first_vertex', 'vertices'])
+
+
+def build_nfa(reg):
     """
     build NFA
     """
     stack = []
     vertices = []
-    first_vertex = 0
-    for i in range(len(reg)):
-        if reg[i].isalpha() or reg[i] == '1':
-            u = len(vertices)
-            v = u + 1
-            vertices.append([[reg[i], v]])
+    for symbol in reg:
+        if symbol.isalpha() or symbol == '1':
+            start = len(vertices)
+            end = start + 1
+            vertices.append([Edge(symbol, end)])
             vertices.append([])
-            stack.append([u, v])
+            stack.append(Pair(start, end))
         else:
-            if reg[i] == '.':
+            if symbol == '.':
                 if len(stack) < 2:
-                    raise RPNError(reg, "Error in the Reverse Polish "
-                                        "notation: . is a binary operation")
+                    raise RPNError(reg, ". is a binary operation")
                 second = stack.pop()
                 first = stack.pop()
-                vertices[first[1]].append(['1', second[0]])
-                stack.append([first[0], second[1]])
+                vertices[first.end].append(Edge('1', second.start))
+                stack.append(Pair(first.start, second.end))
             else:
-                if reg[i] == '+':
+                if symbol == '+':
                     if len(stack) < 2:
-                        raise RPNError(reg, "Error in the Reverse Polish "
-                                       "notation: + is a binary operation")
+                        raise RPNError(reg, "+ is a binary operation")
                     second = stack.pop()
                     first = stack.pop()
-                    vertices[first[0]].append(['1', second[0]])
-                    vertices[first[1]].append(['1', second[1]])
-                    stack.append([first[0], second[1]])
+                    vertices[first.start].append(Edge('1', second.start))
+                    vertices[first.end].append(Edge('1', second.end))
+                    stack.append(Pair(first.start, second.end))
                 else:
-                    if reg[i] == '*':
+                    if symbol == '*':
                         if len(stack) < 1:
-                            raise RPNError(reg, "Error in the Reverse Polish "
-                                           "notation: + is an unary operation")
+                            raise RPNError(reg, "* is an unary operation")
                         last = stack.pop()
-                        vertices[last[1]].append(['1', last[0]])
-                        stack.append([last[0], last[0]])
+                        vertices[last.end].append(Edge('1', last.start))
+                        stack.append(Pair(last.start, last.start))
                     else:
-                        raise RPNError(reg, "Error in the Reverse Polish "
-                                       "notation: unknown symbol")
+                        raise RPNError(reg, "unknown symbol")
     if len(stack) != 1:
-        raise RPNError(reg, "Error in the Reverse Polish notation: more than "
-                       "1 element in the stack at the end of the expression")
+        raise RPNError(reg, "more than 1 element in the stack at the "
+                            "end of the expression")
+    return vertices
+
+
+def delete_empty_edges(vertices):
     """
     delete empty edges
     """
-    for u in range(len(vertices)):
+    first_vertex = 0
+    for start in range(len(vertices)):
         i = 0
-        while i < len(vertices[u]):
-            edge = vertices[u][i]
-            if edge[0] == '1':
-                vertices[u].remove(edge)
-                v = edge[1]
-                if u != v:
-                    if first_vertex == v:
-                        first_vertex = u
-                    vertices[u].extend(vertices[v])
-                    vertices[v].clear()
+        while i < len(vertices[start]):
+            edge = vertices[start][i]
+            if edge.symbol == '1':
+                vertices[start].remove(edge)
+                end = edge.to
+                if start != end:
+                    if first_vertex == end:
+                        first_vertex = start
+                    vertices[start].extend(vertices[end])
+                    vertices[end].clear()
                     for a in range(len(vertices)):
-                        for b in range(len(vertices[a])):
-                            if vertices[a][b][1] == v:
-                                vertices[a][b][1] = u
+                        j = 0
+                        while j < len(vertices[a]):
+                            a_edge = vertices[a][j]
+                            if a_edge.to == end:
+                                vertices[a].remove(a_edge)
+                                vertices[a].append(Edge(a_edge.symbol, start))
+                            else:
+                                j += 1
             else:
                 i += 1
+    return NFA(first_vertex, vertices)
+
+
+def build_dfa(nfa):
     """
     build DFA
     """
+    first_vertex = nfa.first_vertex
+    vertices = nfa.vertices
     classes = [[first_vertex]]
     automaton = []
     i = 0
     while i < len(classes):
-        edges = dict()
+        edges = {}
         for vertex in classes[i]:
             for edge in vertices[vertex]:
-                if edge[0] not in edges.keys():
-                    edges[edge[0]] = []
-                if edge[1] not in edges[edge[0]]:
-                    edges[edge[0]].append(edge[1])
-        automaton.append(dict())
+                if edge.symbol not in edges.keys():
+                    edges[edge.symbol] = []
+                if edge.to not in edges[edge.symbol]:
+                    edges[edge.symbol].append(edge.to)
+        automaton.append({})
         for symbol in edges.keys():
             edges[symbol].sort()
             if edges[symbol] not in classes:
@@ -103,12 +120,21 @@ def build_automaton(reg):
     return automaton
 
 
-def find_prefixes_in_automaton(dfa, string):
+def build_automaton(reg):
+    if reg is '':
+        return [{}]
+    nfa = build_nfa(reg)
+    nfa = delete_empty_edges(nfa)
+    automaton = build_dfa(nfa)
+    return automaton
+
+
+def find_prefixes_in_automaton(automaton, string):
     t = 0
     i = 0
     for i in range(len(string)):
-        if string[i] in dfa[t].keys():
-            t = dfa[t][string[i]]
+        if string[i] in automaton[t].keys():
+            t = automaton[t][string[i]]
         else:
             break
     return i
--- test_automaton.py
+++ test_automaton.py
@@ -3,47 +3,63 @@ import unittest
 
 
 class TestAutomaton(unittest.TestCase):
+    def test_build_nfa(self):
+        nfa = automaton.build_nfa('ab+')
+        for i in range(len(nfa)):
+            for j in range(len(nfa[i])):
+                nfa[i][j] = (nfa[i][j].symbol, nfa[i][j].to)
+        self.assertEqual(nfa, [[('a', 1), ('1', 2)], [('1', 3)], [('b', 3)], []])
 
-    def test_find_prefixes_1(self):
-        self.assertEqual(automaton.find_prefixes('ab+c.aba.*.bac.+.+*',
-                                                 'abacb'), 4)
+    def test_delete_empty_edges(self):
+        nfa = [[automaton.Edge('a', 1)],
+               [automaton.Edge('1', 2)],
+               [automaton.Edge('b', 3)],
+               []]
+        nfa = automaton.delete_empty_edges(nfa)
+        self.assertEqual(nfa.first_vertex, 0)
+        nfa = nfa.vertices
+        for i in range(len(nfa)):
+            for j in range(len(nfa[i])):
+                nfa[i][j] = (nfa[i][j].symbol, nfa[i][j].to)
+        self.assertEqual(nfa, [[automaton.Edge('a', 1)], [automaton.Edge('b', 3)], [], []])
 
-    def test_find_prefixes_2(self):
-        self.assertEqual(automaton.find_prefixes('acb..bab.c.*.ab.ba.+.+*a.',
-                                                 'acbac'), 4)
+    def test_build_dfa(self):
+        nfa = automaton.NFA(
+            0,
+            [[automaton.Edge('a', 1),
+              automaton.Edge('a', 2)],
+             [automaton.Edge('b', 2)],
+             [automaton.Edge('b', 3)],
+             []]
+        )
+        self.assertEqual(automaton.build_dfa(nfa), [{'a': 1}, {'b': 2}, {'b': 3}, {}])
 
-    def test_find_prefixes_3(self):
-        self.assertEqual(automaton.find_prefixes('aba.+*b.a.aab.+*.',
-                                                 'ababba'), 4)
-
-    def test_find_prefixes_4(self):
+    def test_find_prefixes(self):
+        self.assertEqual(automaton.find_prefixes('ab+c.aba.*.bac.+.+*', 'abacb'), 4)
+        self.assertEqual(automaton.find_prefixes('acb..bab.c.*.ab.ba.+.+*a.', 'acbac'), 4)
+        self.assertEqual(automaton.find_prefixes('acb..bab.c.*.ab.ba.+.+*a.', 'acbac'), 4)
+        self.assertEqual(automaton.find_prefixes('aba.+*b.a.aab.+*.', 'ababba'), 4)
         self.assertEqual(automaton.find_prefixes('aba.+*b.a.aab.+*.', 'c'), 0)
-
-    def test_find_prefixes_5(self):
         self.assertEqual(automaton.find_prefixes('1b+*', 'bbab'), 2)
 
     def test_find_prefixes_wrong_regular(self):
         with self.assertRaises(automaton.RPNError):
             automaton.find_prefixes('a+ba.+*b.a.aab.+*.', 'c')
-
+    
     def test_build_automaton_empty_regular(self):
         self.assertEqual(automaton.build_automaton(''),
                          automaton.build_automaton('1'))
 
-    def test_build_automaton_1(self):
+    def test_build_automaton(self):
         self.assertEqual(
             automaton.build_automaton('ab+*a.bc+a.*+'),
             [{'a': 0, 'b': 1, 'c': 2}, {'a': 0, 'b': 1, 'c': 2}, {'a': 0}])
 
-    def test_build_automaton_wrong_regular_1(self):
+    def test_build_automaton_wrong_regular(self):
         with self.assertRaises(automaton.RPNError):
             automaton.build_automaton('b+c.aba.*.bac.+.+*')
-
-    def test_build_automaton_wrong_regular_2(self):
         with self.assertRaises(automaton.RPNError):
             automaton.build_automaton('ab+c.aba/.*.bac.+.+*')
-
-    def test_build_automaton_wrong_regular_3(self):
         with self.assertRaises(automaton.RPNError):
             automaton.build_automaton('*ab+c.aba.*.bac.+.+*')
 
